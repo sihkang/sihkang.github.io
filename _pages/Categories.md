7d051efa3d44ec1c64e64bb72924900063d2ca72
sihkang <sihkang@42seoul.student.co.kr>
2024-06-03 18:27:36 +0900

    z

---
+++ _pages/Categories.md
@@ -0,0 +1,11 @@
+---
+permalink: /Categories/
+title: "Categories"
+toc: true
+toc_sticky: true
+toc_label: "Category"
+---
+
+![icon](/assets/logo.ico/apple-icon-120x120.png)
+
+카테고리 항목이다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "[Jekyll] 블로그 포스팅하는 방법",
-        "excerpt":"  ","categories": ["Blog"],
+        "title": "[시작] Hello, World!",
+        "excerpt":"안녕하세요!     깃 블로그는 마크다운 형식으로 글이 작성됩니다! 따라서 앞으로 사용할 옵시디언과도 궁합이 잘 맞겠ㄱ군요    아주 재밌습니다…야도..샨…     ","categories": ["Blog"],
         "tags": ["Blog","jekyll","Github","Git"],
         "url": "/blog/Hello-World!/",
+        "teaser": null
+      },{
+        "title": "[test] 포스팅을 테스트중입니다.",
+        "excerpt":"안녕하세요!     깃 블로그는 마크다운 형식으로 글이 작성됩니다! 따라서 앞으로 사용할 옵시디언과도 궁합이 잘 맞겠ㄱ군요    아주 재밌습니다…야도..샨…  ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/posting-test/",
         "teaser": null
       }]
